a27fe9234231cfb25f54b87eb3e1dab9e6c8e67d
Nicolas Kruchten <nicolas@kruchten.com>
2015-10-09 11:19:35 -0400

    Update resource.py

--- pymldb/resource.py
+++ pymldb/resource.py
@@ -3,10 +3,14 @@ from pymldb.util import add_repr_html_to_response
 
 class ResourceError(Exception):
     def __init__(self, r):
+        try:
+            message = json.dumps(r.json(), indent=2)
+        except:
+            message = r.content
         super(ResourceError, self).__init__(
-        "'%d %s' response to '%s %s'\n\n%s" % 
-            (r.status_code, r.reason, r.request.method, r.request.url,
-            json.dumps(r.json(), indent=2)))
+            "'%d %s' response to '%s %s'\n\n%s" %
+            (r.status_code, r.reason, r.request.method, r.request.url,message)
+        )
         self.result = r
 
 def decorate_response(fn):
@@ -23,54 +27,36 @@ def decorate_response(fn):
 
 class Resource(object):
     def __init__(self, uri="http://localhost"):
-        if not uri.startswith("http"): 
+        if not uri.startswith("http"):
             raise Exception("URIs must start with 'http'")
         self.uri = uri.strip("/")
-    
+
     def __repr__(self):
         return self.uri
 
     def __str__(self):
         return self.uri
-    
+
     def __call__(self, frag):
         return Resource(self.uri+"/"+str(frag).strip("/"))
-                                              
-    def __getattr__(self, frag): 
+
+    def __getattr__(self, frag):
         return Resource(self.uri+"/"+str(frag).strip("/"))
-    
-    @decorate_response
-    def get(self, *args, **kwargs): 
-        return requests.get(self.uri, **kwargs)
-    
-    @decorate_response
-    def get_query(self, *args, **kwargs): 
-        return requests.get(self.uri, 
-            params= {k: (json.dumps(v) if isinstance(v, dict) else v) 
-                 for k,v in kwargs.items() })
-    
-    @decorate_response
-    def put(self, *args, **kwargs): 
-        return requests.put(self.uri, **kwargs)
-    
-    @decorate_response
-    def delete_and_put_json(self, payload): 
-        self.delete()
-        return requests.put(self.uri, data=json.dumps(payload))
 
     @decorate_response
-    def put_json(self, payload): 
-        return requests.put(self.uri, data=json.dumps(payload))
-    
+    def get(self, **kwargs):
+        return requests.get(self.uri,
+            params= {k: (json.dumps(v) if isinstance(v, dict) else v)
+                 for k,v in kwargs.items() })
+
     @decorate_response
-    def post(self, *args, **kwargs): 
-        return requests.post(self.uri, **kwargs)
-    
+    def put(self, payload):
+        return requests.put(self.uri, json=payload)
+
     @decorate_response
-    def post_json(self, payload): 
-        return requests.post(self.uri, data=json.dumps(payload))
-    
+    def post(self, payload):
+        return requests.post(self.uri, json=payload)
+
     @decorate_response
-    def delete(self, *args, **kwargs): 
-        return requests.delete(self.uri, **kwargs)
-    
+    def delete(self):
+        return requests.delete(self.uri)
